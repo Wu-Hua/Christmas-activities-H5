e033e3bd8f2169216db738c040002f7143a0fc02
华 <569405489@qq.com>
2018-12-20 21:35:53 +0800

    Christmas activities H5

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+	gulp.watch("./*.html").on('change', browserSync.reload);
+	gulp.watch("./css/*.css").on('change', browserSync.reload);
+
+	browserSync.init({
+		server: './'
+	});
+});
